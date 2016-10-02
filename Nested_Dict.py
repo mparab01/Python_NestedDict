b2ffdc7c5a7dcb2d3678c8f86f6b841dd8ba8900
mparab01 <infoeastindia1@gmail.com>
2016-10-01 21:31:21 -0500

    Python_NestedDict
    https://pypi.python.org/pypi/nested_dict

---
+++ Nested_Dict.py
@@ -0,0 +1,42 @@
+
+# coding: utf-8
+
+# In[7]:
+
+d = {'one': 1, 'two':2, 'two':2}
+
+
+# In[8]:
+
+d
+
+
+# In[9]:
+
+d['one']
+
+
+# In[13]:
+
+d1 = {'k1':{'k2':'Hello'}}
+
+
+# In[14]:
+
+d1['k1']['k2']
+
+
+# In[15]:
+
+d2 = {'k1':[{'nest_key':['this is deep', ['hello']]}]}
+
+
+# In[17]:
+
+d2['k1'][0]['nest_key'][1]
+
+
+# In[ ]:
+
+
+
